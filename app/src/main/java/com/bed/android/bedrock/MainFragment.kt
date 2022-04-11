bb9d45bf9bf7a457d9d6fa7b09e14d11aa9b6e8e
acw <acwoo97@gmail.com>
2022-04-11 15:28:36 +0900

    init

---
+++ app/src/main/java/com/bed/android/bedrock/MainFragment.kt
@@ -0,0 +1,39 @@
+package com.bed.android.bedrock
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MainFragment: Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+
+
+
+
+
+
+
+
+    companion object{
+        fun newInstance()=MainFragment()
+    }
+}
---
+++ app/src/main/java/com/bed/android/bedrock/ProductDetailFragment.kt
@@ -0,0 +1,4 @@
+package com.bed.android.bedrock
+
+class ProductDetailFragment {
+}
