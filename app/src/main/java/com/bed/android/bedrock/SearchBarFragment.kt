78062a18a3b147bcf57b46cf2c603b235ef6bc3d
DongHyeon Park <paramore@kakao.com>
2022-04-14 00:19:53 +0900

    1, 2번째 프래그먼트 임시 작성

--- app/src/main/java/com/bed/android/bedrock/SearchBarFragment.kt
+++ app/src/main/java/com/bed/android/bedrock/SearchBarFragment.kt
@@ -1,4 +1,33 @@
 package com.bed.android.bedrock
 
-class SearchBarFragment {
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bed.android.bedrock.databinding.FragmentSearchBarBinding
+
+
+class SearchBarFragment : Fragment() {
+
+    var binding:FragmentSearchBarBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSearchBarBinding.inflate(layoutInflater, container, false)
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding!!.apply {
+
+        }
+    }
+
+    companion object {
+        fun newInstance() = SearchBarFragment()
+    }
 }
