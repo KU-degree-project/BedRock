28f6be7ea1d0791e16d43f370b73d66ffcc4975b
DongHyeon Park <paramore@kakao.com>
2022-04-16 19:43:20 +0900

    첫번째 프래그먼트 전환 애니메이션

--- app/src/main/java/com/bed/android/bedrock/ui/MainFragment.kt
+++ app/src/main/java/com/bed/android/bedrock/ui/MainFragment.kt
@@ -1,9 +1,16 @@
 package com.bed.android.bedrock.ui
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import androidx.core.animation.addListener
+import androidx.core.animation.doOnEnd
 import androidx.fragment.app.Fragment
 import com.bed.android.bedrock.R
 import com.bed.android.bedrock.databinding.FragmentMainBinding
@@ -24,11 +31,29 @@ class MainFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding!!.apply {
+            // 검색창 눌러서 프래그먼트 전환 되기전에 애니메이션
             searchBarView.setOnClickListener {
-                parentFragmentManager.beginTransaction().apply {
-                    replace(R.id.fragment_container, SearchBarFragment.newInstance())
-                    addToBackStack("MainFragment")
-                    commit()
+                searchBarView.text = "" // 안내 텍스트 지우기
+                // 검색창을 위한 Animation
+                ObjectAnimator.ofFloat(searchBarView, "translationY", -((root.height - searchBarView.height) / 2 - TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP, 8f, requireContext().resources.displayMetrics))).apply {
+                        duration = 450
+                        addListener(object:AnimatorListenerAdapter(){
+                            override fun onAnimationEnd(animation: Animator?) {
+                                super.onAnimationEnd(animation)
+                                parentFragmentManager.beginTransaction().apply {
+                                    replace(R.id.fragment_container, SearchBarFragment.newInstance())
+                                    addToBackStack("MainFragment")
+                                    commit()
+                                }
+                            }
+                        })
+                    start() }
+
+                // 타이틀 글자 위로 올려보내기
+                ObjectAnimator.ofFloat(titleText, "translationY", -1000f).apply {
+                    duration = 450
+                    start()
                 }
             }
         }
@@ -39,4 +64,5 @@ class MainFragment: Fragment() {
     companion object{
         fun newInstance() = MainFragment()
     }
+
 }
