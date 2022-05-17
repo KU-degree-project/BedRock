d62e6cf6c09caca83055ab72835bc73bca1655e8
신재혁 <shs0985@gmail.com>
2022-05-17 23:18:25 +0900

    SearchBar MVVM 패턴 적용

--- app/src/main/java/com/bed/android/bedrock/ui/search/SearchBarFragment.kt
+++ app/src/main/java/com/bed/android/bedrock/ui/search/SearchBarFragment.kt
@@ -22,8 +22,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-private const val TAG = "KeywordLoad"
-
 class SearchBarFragment : BaseFragment<FragmentSearchBarBinding>(R.layout.fragment_search_bar) {
 
     interface Callbacks {
@@ -32,23 +30,46 @@ class SearchBarFragment : BaseFragment<FragmentSearchBarBinding>(R.layout.fragme
 
     private var callbacks: Callbacks? = null
     private val viewModel by viewModels<SearchBarViewModel>()
-    private val adapter = SearchRecordAdapter(diffUtil)
-//    private val animation = AnimationUtils.loadAnimation(context, R.anim.fade_inandout)
+    private val adapter = SearchRecordAdapter(diffUtil) {
+        callbacks?.onSearchBtnClicked(it.productName)
+    }
 
-//    private val myPastKeywordViewModel: MyPastKeywordViewModel by lazy {
-//        ViewModelProvider(this).get(MyPastKeywordViewModel::class.java)
-//    }
-//    private val popularKeywordViewModel: PopularKeywordViewModel by lazy {
-//        ViewModelProvider(this).get(PopularKeywordViewModel::class.java)
-//    }
-//    private lateinit var animation: ObjectAnimator
-//    private lateinit var recordRecyclerView: RecyclerView
-//    private var adapter: SearchRecordAdapter? = SearchRecordAdapter(diffUtil)
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
 
-    init {
-        lifecycleScope.launchWhenResumed {
+        if (context is Callbacks) {
+            callbacks = context
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.viewModel = viewModel
+        initView()
+    }
+
+    private fun initView() {
+        viewModel.loadSearchRecord()
+
+        lifecycleScope.launch {
             viewModel.fetchPopularKeyword { list ->
                 startBlinkAnimation(list)
+            }
+        }
+
+        bind {
+            rKeywordRecyclerView.adapter = adapter
+
+            popularKeywordTextView.setOnClickListener {
+                val keyword = popularKeywordTextView.text.split(".")[1].trim()
+                callbacks?.onSearchBtnClicked(keyword)
+            }
+        }
+
+        binding.searchBtn.setOnClickListener {
+            viewModel.addSearchKeyword { keyword ->
+                callbacks?.onSearchBtnClicked(keyword)
             }
         }
     }
@@ -67,164 +88,20 @@ class SearchBarFragment : BaseFragment<FragmentSearchBarBinding>(R.layout.fragme
         }
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
 
-        if (context is Callbacks) {
-            callbacks = context
-        }
-    }
+    class SearchRecordAdapter(
+        diffCallback: DiffUtil.ItemCallback<SearchRecord>,
+        private val onClick: ((SearchRecord) -> Unit)? = null
+    ) : ListAdapter<SearchRecord, SearchRecordHolder>(diffCallback) {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.viewModel = viewModel
-
-        bind {
-            rKeywordRecyclerView.adapter = adapter
-        }
-
-
-//        binding.apply {
-//            animation = ObjectAnimator.ofFloat(binding.popularKeywordTextView, "alpha", 0f, 1f, 1f, 1f, 1f, 0f).apply {
-//                repeatCount = ObjectAnimator.INFINITE
-//                addListener(object: AnimatorListenerAdapter(){
-//                    override fun onAnimationRepeat(animation: Animator?) {
-//                        super.onAnimationRepeat(animation)
-//                        val new_val = popularKeywordViewModel.k_idx.value?.plus(1)?.mod(10)
-//                        popularKeywordViewModel.k_idx.value = new_val
-//                        popularKeywordViewModel.keyword.postValue(popularKeywordViewModel.k_idx.value?.let {
-//                            popularKeywordViewModel.keywords.get(
-//                                it
-//                            )
-//                        })
-//                    }
-//                })
-//                duration = 6000
-//                start()
-//            }
-//        }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // 최근 검색어 불러오기
-//        loadPastKeywords()
-//
-//        // 인기 키워드 불러오기
-//        popularKeywordViewModel.keyword = MutableLiveData()
-//        popularKeywordViewModel.k_idx = MutableLiveData()
-//        GlobalScope.launch(Dispatchers.IO) {
-//            popularKeywordViewModel.keywords = croller.croll_keywords()
-//            popularKeywordViewModel.k_idx.postValue(0)
-//            popularKeywordViewModel.keyword.postValue(popularKeywordViewModel.keywords.get(0))
-//        }
-    }
-
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        binding = FragmentSearchBarBinding.inflate(layoutInflater, container, false)
-//        binding.apply {
-//            searchBtn.setOnClickListener {
-//                addNewKeyword()
-//                callbacks?.onSearchBtnClicked(searchBarEditText.text.toString())
-//            }
-//
-//            popularKeywordTextView.setOnClickListener {
-//                val texts = popularKeywordTextView.text.split(".")[1]
-//                callbacks?.onSearchBtnClicked(texts.trim())
-//            }
-//        }
-//
-//        binding.viewModel = popularKeywordViewModel
-//        binding.lifecycleOwner = viewLifecycleOwner
-//        popularKeywordViewModel.keyword.observe(
-//            viewLifecycleOwner,
-//            Observer {
-//                it?.let {
-//                    binding.popularKeywordTextView.text = it
-//                }
-//            }
-//        )
-//        myPastKeywordViewModel.keyAndDate.observe(
-//            viewLifecycleOwner,
-//            Observer {
-//                it?.let {
-//                    updateUI(it.toMutableList())
-//                }
-//            }
-//        )
-//
-//        recordRecyclerView = binding.rKeywordRecyclerView
-//        recordRecyclerView.layoutManager = LinearLayoutManager(context)
-//        recordRecyclerView.adapter = adapter
-//
-//        return binding.root
-//    }
-
-//    private fun updateUI(keyAndDate: MutableList<SearchRecord>) {
-//        adapter?.submitList(keyAndDate)
-//    }
-
-//    private fun loadPastKeywords() {
-//        val sharedPref = activity?.getSharedPreferences(
-//            getString(R.string.keyword_info), Context.MODE_PRIVATE
-//        ) ?: return
-//        val keywords = sharedPref.getString("keywords", null)
-//        keywords?.let {
-//            val jsonArray = JSONArray(keywords)
-//            for (i in 0 until jsonArray.length()) {
-//                val tempObj = jsonArray.getJSONObject(i)
-//                myPastKeywordViewModel.addRecord(SearchRecord(tempObj.getInt("id"), tempObj.getString("name"), tempObj.getString("date")))
-//            }
-//        }
-//        Log.d(TAG, myPastKeywordViewModel.keyAndDate.value.toString())
-//    }
-
-//    private fun saveKeywords() {
-//        val sharedPref = activity?.getSharedPreferences(
-//            getString(R.string.keyword_info), Context.MODE_PRIVATE
-//        ) ?: return
-//        with(sharedPref) {
-//            val jsonArray = JSONArray()
-//            val keyAndDate = myPastKeywordViewModel.keyAndDate.value
-//            keyAndDate?.let {
-//                for (i in keyAndDate.indices) {
-//                    val tempObj = JSONObject()
-//                    tempObj.put("id", keyAndDate[i].recordId)
-//                    tempObj.put("name", keyAndDate[i].productName)
-//                    tempObj.put("date", keyAndDate[i].searchDate)
-//                    jsonArray.put(tempObj)
-//                }
-//            }
-//
-//            with(edit()){
-//                putString("keywords", jsonArray.toString())
-//                apply()
-//            }
-//        }
-//    }
-
-//    private fun addNewKeyword() {
-//        with (binding) {
-//            val keyword = searchBarEditText.text.toString()
-//            val date = DateFormat.format(getString(R.string.date_foramt), Calendar.getInstance().time).toString()
-//            myPastKeywordViewModel.addRecord(SearchRecord(myPastKeywordViewModel.keyAndDate.value?.size ?: 0, keyword, date))
-//        }
-//    }
-
-
-    class SearchRecordAdapter(diffCallback: DiffUtil.ItemCallback<SearchRecord>)
-        : ListAdapter<SearchRecord, SearchRecordHolder>(diffCallback) {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchRecordHolder {
             val binding = ListItemSearchrecordBinding.inflate(
                 LayoutInflater.from(parent.context),
                 parent,
-                false)
+                false
+            )
 
-            return SearchRecordHolder(binding)
+            return SearchRecordHolder(binding, onClick)
         }
 
         override fun onBindViewHolder(holder: SearchRecordHolder, position: Int) {
@@ -234,19 +111,14 @@ class SearchBarFragment : BaseFragment<FragmentSearchBarBinding>(R.layout.fragme
 
     class SearchRecordHolder(
         private val binding: ListItemSearchrecordBinding,
-        private val onClick: ((SearchRecord) -> Unit)? = null)
-        : RecyclerView.ViewHolder(binding.root) {
+        private val onClick: ((SearchRecord) -> Unit)? = null
+    ) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(searchRecord: SearchRecord) {
-
+            binding.root.setOnClickListener { onClick?.invoke(searchRecord) }
+            binding.item = searchRecord
         }
     }
-
-//    override fun onDestroyView() {
-//        super.onDestroyView()
-//        animation.cancel()
-//        saveKeywords()
-//    }
 
 
     companion object {
--- app/src/main/java/com/bed/android/bedrock/ui/search/SearchBarViewModel.kt
+++ app/src/main/java/com/bed/android/bedrock/ui/search/SearchBarViewModel.kt
@@ -1,43 +1,46 @@
 package com.bed.android.bedrock.ui.search
 
+
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.bed.android.bedrock.di.Injector
 import com.bed.android.bedrock.model.SearchRecord
 import com.bed.android.bedrock.ui.MainActivity.Companion.croller
+import com.bed.android.bedrock.util.TextUtils
 import kotlinx.coroutines.*
 import org.json.JSONArray
+import org.json.JSONObject
 
 class SearchBarViewModel : ViewModel() {
 
     private val sharedPreferences = Injector.getSharedPreferences()
 
-    private val _searchRecord = MutableLiveData<List<SearchRecord>>()
+    val searchKeyword = MutableLiveData("")
+
+    private val _searchRecord = MutableLiveData<List<SearchRecord>>(emptyList())
     val searchRecord: LiveData<List<SearchRecord>>
         get() = _searchRecord
 
     private var popularKeywordJob: Job? = null
 
-    init {
-        loadSearchRecord()
-    }
-
-    private fun loadSearchRecord() {
+    fun loadSearchRecord() {
         val list = mutableListOf<SearchRecord>()
 
         val keywords = sharedPreferences.getString("keywords", null) ?: return
 
         val jsonArray = JSONArray(keywords)
 
-        (0 until jsonArray.length()).forEach {
-            val json = jsonArray.getJSONObject(it)
+        for (i in 0 until jsonArray.length()) {
+            val json = jsonArray.getJSONObject(i)
 
-            list.add(SearchRecord(
-                json.getInt("id"),
-                json.getString("name"),
-                json.getString("date")
-            ))
+            list.add(
+                SearchRecord(
+                    json.getInt("id"),
+                    json.getString("name"),
+                    json.getString("date")
+                )
+            )
         }
 
         _searchRecord.postValue(list)
@@ -50,6 +53,39 @@ class SearchBarViewModel : ViewModel() {
             withContext(Dispatchers.Main) {
                 callback(list)
             }
+        }
+    }
+
+    fun addSearchKeyword(callback: (String) -> Unit) {
+        searchKeyword.value?.let { keyword ->
+            if (keyword.isBlank()) return
+
+            val id = _searchRecord.value?.size ?: 0
+            val record = JSONObject().apply {
+                put("id", id)
+                put("name", keyword)
+                put("date", TextUtils.getFormattedDate())
+            }
+
+            val jsonArray = JSONArray()
+
+            _searchRecord.value?.forEach { searchRecord ->
+                JSONObject().apply {
+                    put("id", searchRecord.recordId)
+                    put("name", searchRecord.productName)
+                    put("date", searchRecord.searchDate)
+                    jsonArray.put(this)
+                }
+            }
+
+            jsonArray.put(record)
+
+            sharedPreferences.edit().apply {
+                putString("keywords", jsonArray.toString())
+                apply()
+            }
+
+            callback(keyword)
         }
     }
 
