0abf08e1c9c81b3b3ac97185a8496c713ecb7e96
신재혁 <shs0985@gmail.com>
2022-05-18 23:41:04 +0900

    SearchBarFragment Adapter 클래스 분리

--- app/src/main/java/com/bed/android/bedrock/ui/search/SearchBarFragment.kt
+++ app/src/main/java/com/bed/android/bedrock/ui/search/SearchBarFragment.kt
@@ -17,6 +17,7 @@ import com.bed.android.bedrock.databinding.FragmentSearchBarBinding
 import com.bed.android.bedrock.databinding.ListItemSearchrecordBinding
 import com.bed.android.bedrock.model.SearchRecord
 import com.bed.android.bedrock.ui.BaseFragment
+import com.bed.android.bedrock.ui.adapter.SearchRecordAdapter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -85,38 +86,6 @@ class SearchBarFragment : BaseFragment<FragmentSearchBarBinding>(R.layout.fragme
                     text = list[it % 10]
                 }
             }
-        }
-    }
-
-
-    class SearchRecordAdapter(
-        diffCallback: DiffUtil.ItemCallback<SearchRecord>,
-        private val onClick: ((SearchRecord) -> Unit)? = null
-    ) : ListAdapter<SearchRecord, SearchRecordHolder>(diffCallback) {
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchRecordHolder {
-            val binding = ListItemSearchrecordBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-
-            return SearchRecordHolder(binding, onClick)
-        }
-
-        override fun onBindViewHolder(holder: SearchRecordHolder, position: Int) {
-            holder.bind(getItem(position))
-        }
-    }
-
-    class SearchRecordHolder(
-        private val binding: ListItemSearchrecordBinding,
-        private val onClick: ((SearchRecord) -> Unit)? = null
-    ) : RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(searchRecord: SearchRecord) {
-            binding.root.setOnClickListener { onClick?.invoke(searchRecord) }
-            binding.item = searchRecord
         }
     }
 
